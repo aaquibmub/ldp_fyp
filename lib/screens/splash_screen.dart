40bb350c19cc2f5c0104b95b41ffba905f56dc24
Aaquib Mubarak <aaquib.mubarak@gmail.com>
2023-11-30 10:33:19 +0500

    splash screen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../helpers/common/constants.dart';
+
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Constants.backgroundColor,
       body: Center(
         child: Text('loading..'),
       ),
